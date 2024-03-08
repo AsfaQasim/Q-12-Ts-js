30b64b56739ae5982dbdadb8ad8f01b82b93dac6
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-07 17:36:00 -0800

    Q 12 of Ts/js & node js of 45 assignment
    Q 12 of Ts/js & node js of 45 assignment

---
+++ main.js
@@ -0,0 +1,6 @@
+//Exercise 12
+var members = ["Faria", "Sundas", "Iqra", "Nazish"];
+var message = "kal birthday hai kis ki:";
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ main.ts
@@ -0,0 +1,7 @@
+//Exercise 12
+let members: string[] =[`Faria`,`Sundas`,`Iqra`,`Nazish`];
+let message: string= "kal birthday hai kis ki:";
+for(let i=0; i<members.length; i++)
+{
+    console.log(message + members[i]);
+}
